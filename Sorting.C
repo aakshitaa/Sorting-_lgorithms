d2d55a9ce3bb753634857e85664866a681b33f0e
aakshitaa <aakshitasingh786@gmail.com>
2022-09-22 21:56:24 +0530

    this contains sorting algorithms

---
+++ Sorting.C
@@ -0,0 +1,44 @@
+#include <stdio.h>
+int partition(int arr[], int low, int high)
+	{
+		int pivot = arr[high];
+		int i = (low - 1);
+
+		for (int j = low; j <= high - 1; j++) {
+			if (arr[j] < pivot) {
+				i++;
+				int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+			}
+		}
+		int temp = arr[i+1];
+        arr[i] = arr[high];
+        arr[high] = temp;
+		return (i + 1);
+	}
+
+static void quickSort(int arr[], int low, int high)
+	{
+		if (low < high) {
+			int pi = partition(arr, low, high);
+			quickSort(arr, low, pi - 1);
+			quickSort(arr, pi + 1, high);
+		}
+	}
+
+	static void printArray(int arr[], int size)
+	{
+		for (int i = 0; i < size; i++)
+			printf("%d ",arr[i]);
+	}
+
+	int main()
+	{
+		int arr[] = { 10, 7, 8, 9, 1, 5 };
+		int n = 6;
+		quickSort(arr, 0, n - 1);
+		printf("Sorted array: ");
+		printArray(arr, n);
+	}
+
